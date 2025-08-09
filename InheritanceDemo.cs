5aefefc854cf16e264b45fb96058314c557d1e94
Davelar1789 <dnalaryea003@st.ug.edu.gh>
2025-08-09 06:50:19 +0000

    inheritance

---
+++ InheritanceDemo.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace DCIT318_Assignment2
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    public static class InheritanceDemo
+    {
+        public static void Run()
+        {
+            Console.WriteLine("\n--- Inheritance and Method Overriding ---");
+
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
